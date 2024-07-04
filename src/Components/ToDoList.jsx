8a0b517f34f07dd35a59264c106f38fb8cd5aea3
PugazharasanC <pugazharasankct@gmail.com>
2024-07-04 13:09:08 +0530

    Added:axios

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -2,71 +2,67 @@ import { useEffect, useReducer } from "react";
 import AddTodo from "./AddTodo";
 import ToDo from "./ToDo";
 import ToDoReducer from "../Reducers/ToDoReducer";
+import axios from "axios";
 
 const ToDoList = () => {
   const [todos, dispatch] = useReducer(ToDoReducer, []);
 
   useEffect(() => {
-    fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos")
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "INIT_TODOS", payload: data });
+    axios
+      .get("/")
+      // .then((res) => res.json())
+      .then((res) => {
+        // console.log(data)
+        dispatch({ type: "INIT_TODOS", payload: res.data });
       });
   }, []);
 
   const addTodos = (todo) => {
-    fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(todo),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "ADD_TODO", payload: data });
-      });
+    axios.post("/", todo).then((res) => {
+      // console.log(res);
+      dispatch({ type: "ADD_TODO", payload: res.data });
+    });
   };
 
   const toggleCompleted = (id) => {
     const todo = todos.filter((todo) => todo.id == id)[0];
     todo.completed = !todo.completed;
-    fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos/" + id, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(todo),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "UPDATE_TODO", payload: data });
-      });
+    // fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos/" + id, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(todo),
+    // })
+    // .then((res) => res.json())
+    axios.put("/" + id, todo).then((res) => {
+      dispatch({ type: "UPDATE_TODO", payload: res.data });
+    });
   };
   const deleteTodo = (id) => {
-    fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos/" + id, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "DELETE_TODO", payload: data.id });
-      });
+    // fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos/" + id, {
+    //   method: "DELETE",
+    // })
+    //   .then((res) => res.json())
+    axios.delete("/" + id).then((res) => {
+      dispatch({ type: "DELETE_TODO", payload: res?.data?.id });
+    });
     // dispatch({ type: "DELETE_TODO", payload: id });
   };
   const updateTodo = (id, text) => {
     const todo = todos.filter((todo) => todo.id == id)[0];
     todo.text = text;
-    fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos/" + id, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(todo),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "UPDATE_TODO", payload: data });
-      });
+    // fetch("https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos/" + id, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(todo),
+    // })
+    //   .then((res) => res.json())
+    axios.put("/" + id, todo).then((res) => {
+      dispatch({ type: "UPDATE_TODO", payload: res.data });
+    });
   };
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import axios from "axios";
+axios.defaults.baseURL =
+  "https://6671155ae083e62ee439f74b.mockapi.io/api/v1/todos";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
